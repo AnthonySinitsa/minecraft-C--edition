ab59f999b3efefc4a575e93d5d5a3f2bccd62bc5
AnthonySinitsa <itonyemail@gmail.com>
2023-10-31 04:18:21 +0000

    mode height and width change

--- src/main.cpp
+++ src/main.cpp
@@ -33,9 +33,9 @@ int main() {
     }
 
     // Create a windowed mode window and its OpenGL context
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    // glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     // Make the window's context current
     glfwMakeContextCurrent(window);
@@ -47,7 +47,7 @@ int main() {
     }
 
     // Set the viewport
-    glViewport(0, 0, 800, 600);
+    glViewport(0, 0, mode->width, mode->height);
 
     // Main loop
     while (!glfwWindowShouldClose(window)) {
