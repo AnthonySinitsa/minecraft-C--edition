59fb92808cf84372d1496e67c222ec5b6443041f
AnthonySinitsa <itonyemail@gmail.com>
2023-11-04 06:48:41 +0000

    fullescreen fix lol

--- include/Window.h
+++ include/Window.h
@@ -10,16 +10,12 @@ namespace MinecraftClone
 		int windowWidth;
 		int windowHeight;
 
-    // Constructor to initialize the members
-    Window(int width, int height, GLFWwindow* window)
-        : windowWidth(width), windowHeight(height), nativeWindow(window) {}
-
 		void installMainCallbacks();
 
 		void close();
 
     bool isFullScreen();
-    void setFulleScreen(bool fs);
+    void setFullScreen(bool fs);
 	
 		static Window* createWindow(int width, int height, const char* title, bool fullScreenMode=false);
 		static void freeWindow(Window* window);
