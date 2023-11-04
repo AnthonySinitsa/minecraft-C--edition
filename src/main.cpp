8883281a0cae7a112789931320399d2737c4ca3b
AnthonySinitsa <itonyemail@gmail.com>
2023-11-04 05:36:40 +0000

    handling escape closing window and quality of life code cahgnes

--- src/main.cpp
+++ src/main.cpp
@@ -35,32 +35,41 @@ int main() {
     // Load all OpenGL function pointers using GLAD
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
         std::cerr << "Failed to initialize GLAD" << std::endl;
+        glfwDestroyWindow(window);
+        glfwTerminate();
         return -1;
     }
 
     // Set the viewport
     glViewport(0, 0, 800, 600);
 
-    glfwSetKeyCallback(window, MinecraftClone::Input::keyCallback);
-    glfwSetCursorPosCallback(window, MinecraftClone::Input::mouseCallback);
-    glfwSetMouseButtonCallback(window, MinecraftClone::Input::mouseButtonCallback);
-    glfwSetScrollCallback(window, MinecraftClone::Input::mouseScrollCallback);
+    glfwSetKeyCallback(window, keyCallback);
+    glfwSetCursorPosCallback(window, mouseCallback);
+    glfwSetMouseButtonCallback(window, mouseButtonCallback);
+    glfwSetScrollCallback(window, mouseScrollCallback);
 
     // Main loop
     while (!glfwWindowShouldClose(window)) {
+        // Clear buffer
+        glClearColor(0.98f, 0.47f, 0.43f, 1.0f); // Soft red color
+        glClear(Gl_COLOR_BUFFER_BIT);
+
+
         // Handle key press
-        if (MinecraftClone::Input::isKeyDown(GLFW_KEY_W)) {
-            std::cout << "Key W is being pressed." << std::endl;
+        if (isKeyDown(GLFW_KEY_E)) {
+            std::cout << "Key E is being pressed." << std::endl;
             // Additional handling for W key press
         }
 
-        // Handle mouse button press
-        if (MinecraftClone::Input::isMouseButtonDown(GLFW_MOUSE_BUTTON_LEFT)) {
-            std::cout << "Left mouse button is being pressed." << std::endl;
-            // Additional handling for left mouse button press
+        // Check mouse position
+        if (mouseX >= windowWidth / 2.0f){
+            std::cout << "Mouse is at right part of window." << std::endl;
         }
 
-        // Render here
+        // Check for escape key to close window
+        if (isKeyDown(GLFW_KEY_ESCAPE)){
+            glfwSetWindowShouldClose(window, GLFW_TRUE);
+        }
 
         // Swap front and back buffers
         glfwSwapBuffers(window);
@@ -68,6 +77,7 @@ int main() {
         glfwPollEvents();
     }
 
+    glfwDestroyWindow(window);
     glfwTerminate(); // Clean up and close the application
     return 0;
 }
